fb80c32dba8ddf5fa35b155a5ba2f71326170831
novaTopFlex <andrewkaymars@proton.me>
2024-05-04 08:57:22 -0400

    Update Combined-Max.sh

--- Combined-Max.sh
+++ Combined-Max.sh
@@ -1,1 +1,1 @@
-dialog --yesno "Continue?" 0 0 && { { ps aemuwwxZ; netstat -aeenopvW; top -bn 1; } | { less; wc; } }
+dialog --yesno "Continue?" 0 0 && { { ps aemuwwxZ; netstat -aeenopvW; top -bn 1; } | { less; wc; } } : echo $? # Artifact Warning for Terminal Emulators; standard tty interfaces not impacted.
