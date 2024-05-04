d47911612e1516d43e77f564c8d02e047ba0a3ba
novaTopFlex <andrewkaymars@proton.me>
2024-05-04 08:54:53 -0400

    Update Combined-Max.sh

--- Combined-Max.sh
+++ Combined-Max.sh
@@ -1,1 +1,1 @@
-dialog --yesno "Continue?" 0 0 && { { ps aemuwwxZ; netstat -aeenopvW; top -bn 1; } | { less; wc; } } ; echo $?
+dialog --yesno "Continue?" 0 0 && { { ps aemuwwxZ; netstat -aeenopvW; top -bn 1; } | { less; wc; } } ; clear; echo $?
