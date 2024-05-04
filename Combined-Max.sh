e3e606d9b578568334d45dd595d9977051842a92
novaTopFlex <andrewkaymars@proton.me>
2024-05-04 09:37:59 -0400

    Update Combined-Max.sh
    ":" is improper bash syntax.

--- Combined-Max.sh
+++ Combined-Max.sh
@@ -1,1 +1,1 @@
-dialog --yesno "Continue?" 0 0 && { { ps aemuwwxZ; netstat -aeenopvW; top -bn 1 -e k -E k; } | { less; wc; } } : echo $? # Artifact Warning for Terminal Emulators; standard tty interfaces not impacted.
+dialog --yesno "Continue?" 0 0 && { { ps aemuwwxZ; netstat -aeenopvW; top -bn 1 -e k -E k; } | { less; wc; } } ; echo $? # Artifact Warning for Terminal Emulators; standard tty interfaces not impacted.
