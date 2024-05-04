69f41a36733b08f8f23a62fad8883b7ff291ac88
novaTopFlex <andrewkaymars@proton.me>
2024-05-04 08:10:51 -0400

    Update Process.sh

--- Process.sh
+++ Process.sh
@@ -1,4 +1,4 @@
 ps aemuwwxZ | less # User-oriented format first run
 ps aemswwxZ | less # Signal format second run; this will appear as a low-level interface.
 ps aemvwwxZ | less # Virtual-memory format third run
-ps aemXwwxZ | less # Register format final run; this is likely the lowest-level interface to `ps`
+ps aemXwwxZ | less # Register format final run; this is likely the lowest-level interface to `ps` (not supported on certain BSD interfaces)
