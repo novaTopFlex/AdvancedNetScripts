74bc2ee2b6e59d3381cdbd86fcfdb73ec378eab2
novaTopFlex <andrewkaymars@proton.me>
2024-05-05 21:31:41 -0400

    Update ProcDumpAdd.sh

--- Forensics/ProcDumpAdd.sh
+++ Forensics/ProcDumpAdd.sh
@@ -1,1 +1,1 @@
-dialog --yesno "The following command may significantly increase the used storage space on your system, depending on the total number of processes to be displayed at a given time." 0 0 && { ps aeux &>> proc.txt; }
+dialog --yesno "The following command may significantly increase the used storage space on your system, depending on the total number of processes to be displayed at a given time." 0 -1 && { ps aeux &>> proc.txt; }
