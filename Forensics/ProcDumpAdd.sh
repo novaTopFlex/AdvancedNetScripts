1ea64368e69c8d13bdc6a9aab4beb4e2d3745243
novaTopFlex <andrewkaymars@proton.me>
2024-05-05 21:30:29 -0400

    Update and rename ProcdumpAdd.sh to ProcDumpAdd.sh

---
+++ Forensics/ProcDumpAdd.sh
@@ -0,0 +1,1 @@
+dialog --yesno "This may significantly increase the used storage space on your system" 0 0 && { ps aeux &>> proc.txt; }
--- Forensics/ProcdumpAdd.sh
+++
@@ -1,1 +0,0 @@
-dialog --yesno "This may significantly increase the used storage space on your system" && { ps aeux &>> proc.txt; }
