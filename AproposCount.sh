1d3634f34988169b6010ceb4ba8166d9455c4e25
novaTopFlex <andrewkaymars@proton.me>
2024-05-12 20:54:11 -0400

    Create AproposCount.sh

---
+++ AproposCount.sh
@@ -0,0 +1,9 @@
+apropos . -m 1 | wc
+apropos . -m 2 | wc
+apropos . -m 3 | wc
+apropos . -m 4 | wc
+apropos . -m 5 | wc
+apropos . -m 6 | wc
+apropos . -m 7 | wc
+apropos . -m 8 | wc
+apropos . -m 9 | wc
