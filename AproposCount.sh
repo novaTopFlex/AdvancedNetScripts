6d3c6d378a78d50b8286b2668400ab88f48f99a2
novaTopFlex <andrewkaymars@proton.me>
2024-05-12 20:56:39 -0400

    Update AproposCount.sh

--- AproposCount.sh
+++ AproposCount.sh
@@ -6,4 +6,4 @@ apropos . -s 5 | wc
 apropos . -s 6 | wc
 apropos . -s 7 | wc
 apropos . -s 8 | wc
-apropos . -s 9 | wc
+apropos . -s 9 | wc # Nonstandard kernel files may not occur or be present on every system. Therefore, you may receive a nonzero exit status and thus return a word count of 0 (standard error is not included).
